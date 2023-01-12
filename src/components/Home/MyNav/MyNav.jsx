d984d9b2f9f040fe511d471d96355ad1dcfac6e9
masudrana08 <rana028511@gmail.com>
2023-01-12 06:49:53 +0600

    responsive navbar

--- src/components/Home/MyNav/MyNav.jsx
+++ src/components/Home/MyNav/MyNav.jsx
@@ -5,13 +5,13 @@ export default function MyNav() {
   return (
     <div className="mynav">
       <input type="checkbox" id="nav-toogler" className="nav-toogler" />
-      <div className='navv'>
+      <div className='navv' >
         <label className="nav-toogler-label" htmlFor="nav-toogler">
           <span></span>
         </label>
         {/* <img src={LogoWithText} alt="Logo" /> */}
         <div className="logo">
-          <h3>LOGO</h3>
+          <h3>Footube.</h3>
         </div>
         <ul className='items'>
         <li>Home</li>
---
+++ src/pages/Test/Test.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './test.css'
+export default function Test() {
+  return (
+    <div className="testCont">
+      <div className="box">
+        <h3>Toy 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorem!</p>
+      </div>
+      <div className="box">
+        <h3>Cat 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorem!</p>
+      </div>
+      <div className="box">
+        <h3>Pet 3</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorem!</p>
+      </div>
+      <div className="box">
+        <h3>Toy 4</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorem!</p>
+      </div>
+      <div className="box">
+        <h3>Cat 5</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorem!</p>
+      </div>
+      <div className="box">
+        <h3>Pet 6</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorem!</p>
+      </div>
+    </div>
+  )
+}
